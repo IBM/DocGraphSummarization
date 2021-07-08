Made some fixes to averaging embedding values in the graph constructor.

src/ot_coarsening/dataset_management/cnndm/cnn_daily_mail.py
@@ -42,11 +42,12 @@ def file_len(fname):
     - I am doing an in memory dataset (the train is 5GB), but it loads into CPU memory so that should be fine. I may need to change that later
 """
 class CNNDailyMail(InMemoryDataset):
-    def __init__(self, transform=None, pre_transform=None, mode="train", graph_constructor=None, perform_processing=False, proportion_of_dataset=1.0, max_number_of_nodes=1000, overwrite_existing=False, reduce_dimensionality=False):
+    def __init__(self, transform=None, pre_transform=None, mode="train", graph_constructor=None, perform_processing=False, proportion_of_dataset=1.0, max_number_of_nodes=1000, reduce_dimensionality=False, dense=False):
         self.root = "/dccstor/helbling1/data/CNNDM"
         self.mode = mode # "trian", "test", or "val"
         self.graph_constructor = graph_constructor
         self.dimensionality = 768
+        self.dense = dense
         self.num_output_sentences = 3
         self.reduce_dimensionality = reduce_dimensionality
         if self.reduce_dimensionality:
@@ -55,7 +56,6 @@ class CNNDailyMail(InMemoryDataset):
             self.dimensionality = self.dimensionality_reducer.reduced_size
         self.proportion_of_dataset = proportion_of_dataset
         self.perform_processing = perform_processing
-        self.overwrite_existing = overwrite_existing
         self.max_number_of_nodes = max_number_of_nodes
         self.max_summary_length = 10
         self.num_sentence_nodes = None
@@ -133,7 +133,7 @@ class CNNDailyMail(InMemoryDataset):
                         label = label_reader.read()
                         # check if file exists
                         save_path = os.path.join(self.root, "processed", self.mode, "data_{}.pt".format(index))
-                        if not self.overwrite_existing and os.path.exists(save_path):
+                        if not self.perform_processing and os.path.exists(save_path):
                             current_index += 1
                             continue
                         # num sentences
@@ -229,7 +229,7 @@ class CNNDailyMail(InMemoryDataset):
         graph.x = new_x
         return graph
     
-    def get(self, idx, dense=True):
+    def get(self, idx):
         data_dir = os.path.join(self.root, "processed", self.mode)
         graph_data_path = os.path.join(data_dir, "data_{}.pt".format(idx))
         # check if path exists
@@ -238,7 +238,7 @@ class CNNDailyMail(InMemoryDataset):
         graph = torch.load(graph_data_path)
         graph = graph.to(device)
 
-        if dense:
+        if self.dense:
             graph = T.ToDense(self.max_number_of_nodes)(graph)
             graph.adj = graph.adj.squeeze().float()
             graph = self.reshape_graph(graph)

src/ot_coarsening/dataset_management/cnndm/process_cnn_dailymail.py
@@ -2,9 +2,8 @@ import os
 import sys
 sys.path.append(os.environ["GRAPH_SUM"])
 # from src.dataset_management.datasets import get_dataset
-from src.ot_coarsening.dataset_management.cnn_daily_mail import CNNDailyMail
+from src.ot_coarsening.dataset_management.cnndm.cnn_daily_mail import CNNDailyMail
 from src.ot_coarsening.dataset_management.graph_constructor import CNNDailyMailGraphConstructor
-from src.ot_coarsening.ot_coarsening import MultiLayerCoarsening
 import torch
 import torch.nn.functional as F
 import time
@@ -15,7 +14,7 @@ from torch.optim import Adam
 import warnings
 warnings.filterwarnings("ignore")
 # setup arguments
-device = 'cpu' #torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda') #torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 """
     Main function for processing the dataset
@@ -25,10 +24,10 @@ def main():
     graph_constructor = CNNDailyMailGraphConstructor()
     # Setup CNNDailyMail data
     mode = "train"
-    proportion_of_dataset = 0.1
+    proportion_of_dataset = 0.01
     dataset = CNNDailyMail(graph_constructor=graph_constructor, perform_processing=True, mode=mode, proportion_of_dataset=proportion_of_dataset)
     mode = "val"
-    proportion_of_dataset = 0.1
+    proportion_of_dataset = 0.01
     dataset = CNNDailyMail(graph_constructor=graph_constructor, perform_processing=True, mode=mode, proportion_of_dataset=proportion_of_dataset)
 
 if __name__ == "__main__":

src/ot_coarsening/dataset_management/graph_constructor.py
@@ -92,64 +92,52 @@ def convert_to_average_duplicates(embeddings, tokens):
     return average_dictionary
 
 """
+    Returns two outputs a list of words and a list of embeddings 
+    corresponding to each word. This includes duplicates of words
+"""
+def compute_bert_word_embeddings_token_pairs(bert_model, bert_tokenizer, sentences, unique_words, word_embedding_size):
+    # perform batch tokenizations
+    batch_encoding = bert_tokenizer(sentences, padding=True)
+    # deduce the tokens from the ids in batch_encoding
+    tokens_list = []
+    for i in range(len(batch_encoding.input_ids)):
+        token_ids = batch_encoding.input_ids[i]
+        tokens =  bert_tokenizer.convert_ids_to_tokens(token_ids)
+        tokens_list.append(tokens)
+    # go through and produce the word embeddings
+    input_ids = torch.LongTensor(batch_encoding.input_ids).to(device)
+    attention_masks = torch.FloatTensor(batch_encoding.attention_mask).to(device)
+    #token_type_ids = torch.LongTensor(batch_encoding.token_type_ids).to(device)
+    base_model_output = bert_model(input_ids=input_ids, attention_mask=attention_masks)#, token_type_ids=token_type_ids)
+    embeddings = base_model_output.last_hidden_state
+    # apply attention mask to embeddings
+    output_embeddings = []
+    output_tokens = []
+    for i, embedding_list in enumerate(embeddings):
+        attention_mask = attention_masks[i] > 0
+        masked_embeddings = torch.masked_select(embedding_list.T, attention_mask).T
+        masked_embeddings = torch.reshape(masked_embeddings, (-1, word_embedding_size))
+        output_embeddings.append(masked_embeddings)
+        # do token mask
+        tokens = []
+        for j, token in enumerate(tokens_list[i]):
+            if attention_mask[j]:
+                tokens.append(token)
+        output_tokens.append(tokens)
+    
+    return output_embeddings, output_tokens
+
+"""
     Generates word embeddings for words in a given sentenece using
     pretrained BERT.
 
     - sentence
 """
-def compute_bert_word_embedding(bert_model, bert_tokenizer, sentences, unique_words, word_embedding_size, longform_embedding):
-
-    """
-       Produces tokenizations for the list of sentences in text
-    """
-    def compute_embeddings(sentences, padding=True):
-        # perform batch tokenizations
-        batch_encoding = bert_tokenizer(sentences, padding=True)
-        # deduce the tokens from the ids in batch_encoding
-        tokens_list = []
-        for i in range(len(batch_encoding.input_ids)):
-            token_ids = batch_encoding.input_ids[i]
-            tokens =  bert_tokenizer.convert_ids_to_tokens(token_ids)
-            tokens_list.append(tokens)
-        # go through and produce the word embeddings
-        input_ids = torch.LongTensor(batch_encoding.input_ids).to(device)
-        attention_masks = torch.FloatTensor(batch_encoding.attention_mask).to(device)
-        #token_type_ids = torch.LongTensor(batch_encoding.token_type_ids).to(device)
-        base_model_output = bert_model(input_ids=input_ids, attention_mask=attention_masks)#, token_type_ids=token_type_ids)
-        embeddings = base_model_output.last_hidden_state
-        # apply attention mask to embeddings
-        output_embeddings = []
-        output_tokens = []
-        for i, embedding_list in enumerate(embeddings):
-            attention_mask = attention_masks[i] > 0
-            masked_embeddings = torch.masked_select(embedding_list.T, attention_mask).T
-            masked_embeddings = torch.reshape(masked_embeddings, (-1, word_embedding_size))
-            output_embeddings.append(masked_embeddings)
-            # do token mask
-            tokens = []
-            for j, token in enumerate(tokens_list[i]):
-                if attention_mask[j]:
-                    tokens.append(token)
-            output_tokens.append(tokens)
-        return output_embeddings, output_tokens
-
-    def compute_longform_embeddings(sentences, padding=True):
-        pass
-
+def compute_bert_word_embedding(bert_model, bert_tokenizer, sentences, unique_words, word_embedding_size):
     words_to_embeddings = {word: [] for word in unique_words}
     # tokenize the sentence (includes punctuation, before and after tokens, and subword tokens)
-    # input_ids_batched = bert_tokenizer(sentences, padding=True)
-    if longform_embedding:
-        output_embeddings, output_tokens = compute_longform_embeddings(sentences, padding=True)
-    else:
-        output_embeddings, output_tokens = compute_embeddings(sentences, padding=True)
-    # input_ids_batched, tokens_batched = compute_tokenizations(sentences, padding=True)
-    # produce embedding values for each token
-    # outputs = model(input_ids_batched)
-    # token_embeddings = outputs[:, 0].squeeze()  # The last hidden-state is the first element of the output tuple
-    # token_embeddings_batched = token_embeddings.detach().cpu()
+    output_embeddings, output_tokens = compute_bert_word_embeddings_token_pairs(bert_model, bert_tokenizer, sentences, unique_words, word_embedding_size)
     # clean up the output
-    embeddings_dicts = []
     for i in range(len(output_embeddings)):
         tokens = output_tokens[i]
         token_embeddings = output_embeddings[i] 
@@ -159,11 +147,16 @@ def compute_bert_word_embedding(bert_model, bert_tokenizer, sentences, unique_wo
         embeddings, tokens = merge_subword_embeddings(embeddings, tokens)
         # remove punctionation
         embeddings, tokens = remove_punctuation(embeddings, tokens)
-        # convert to dictionary and average dupliicates
-        embeddings_dict = convert_to_average_duplicates(embeddings, tokens)
-        embeddings_dicts.append(embeddings_dict)
-    # output a dictionary mapping each unique word to an embedding vector
-    return embeddings_dicts
+        # add each instance of a word's embedding to the embedding list
+        for token_index in range(len(tokens)):
+            token = tokens[token_index]
+            if not token in unique_words:
+                # this is necessary to filter stop words
+                continue
+            token_embedding = token_embeddings[token_index] 
+            words_to_embeddings[token].append(token_embedding)
+
+    return words_to_embeddings
 
 """
     Generates sentence embeddings for a sentence using pretrained
@@ -175,15 +168,28 @@ def compute_bert_sentence_embedding(sentences, model):
     sentence_embeddings = torch.Tensor(sentence_embeddings).to(device)
     return sentence_embeddings
 
+
+class GraphConstructor():
+      
+    def __init__(self):
+        pass
+
+    def construct_graph(self):
+        pass
+
 """
     This is an object that gets passed to a graph dataset that gets used to
     construct a graph
 """
-class GraphConstructor():
+class CNNDailyMailGraphConstructor():
 
     def __init__(self):
+        self.bert_model = DistilBertModel.from_pretrained('distilbert-base-uncased').to(device)
+        self.bert_tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+        # self.sentence_transformer =  SentenceTransformer('paraphrase-mpnet-base-v2')
+        self.sentence_transformer = SentenceTransformer('paraphrase-distilroberta-base-v1')
+        self.word_embedding_size = self.bert_model.config.hidden_size
         self.longform_embedding = False
-        pass
 
     def _get_unique_words(self, tfidf):
         unique_words = set()
@@ -200,19 +206,12 @@ class GraphConstructor():
         return filtered_words
 
     def _get_mean_word_embeddings(self, text, tfidf, unique_words):
-        words_to_embeddings = {word: [] for word in unique_words}
 	    # perform word_embeddinng on the batch
-        # start = time.time()
-        embedding_dicts = compute_bert_word_embedding(self.bert_model, self.bert_tokenizer, text, unique_words, self.word_embedding_size, self.longform_embedding)
-        # finish = time.time()
-        # print("Compute bert word embedding time {}".format(finish - start))
-        for i, word_embeddings_dict in enumerate(embedding_dicts):
-            tfidf_words = list(tfidf[str(i)].keys())
-            # assuming we have a list of tokens and a list of word embeddings
-            for j, token in enumerate(tfidf_words):
-                if token in word_embeddings_dict and token in words_to_embeddings:
-                    word_embedding = word_embeddings_dict[token]
-                    words_to_embeddings[token].append(word_embedding)
+        words_to_embeddings = compute_bert_word_embedding(self.bert_model, 
+                                                      self.bert_tokenizer, 
+                                                      text,
+                                                      unique_words,
+                                                      self.word_embedding_size)
         # mean each list
         mean_embeddings = {word: None for word in unique_words}
         for word in unique_words:
@@ -337,29 +336,105 @@ class GraphConstructor():
 
         return data_object
 
-class CNNDailyMailGraphConstructor(GraphConstructor):
-
-    def __init__(self):
-        self.bert_model = DistilBertModel.from_pretrained('distilbert-base-uncased').to(device)
-        self.bert_tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
-        # self.sentence_transformer =  SentenceTransformer('paraphrase-mpnet-base-v2')
-        self.sentence_transformer = SentenceTransformer('paraphrase-distilroberta-base-v1')
-        self.word_embedding_size = self.bert_model.config.hidden_size
-        self.longform_embedding = False
-
 class DUCGraphConstructor(GraphConstructor):
 
     def __init__(self):
         self.bert_model = DistilBertModel.from_pretrained('distilbert-base-uncased').to(device)
         self.bert_tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
-        # self.sentence_transformer =  SentenceTransformer('paraphrase-mpnet-base-v2')
         self.sentence_transformer = SentenceTransformer('paraphrase-distilroberta-base-v1')
         self.word_embedding_size = self.bert_model.config.hidden_size
         self.longform_embedding = False
 
+    def _get_unique_words(self, tfidf):
+        # go through all the words in the document_tfidf
+        document_tfidf = tfidf["document_tfidf"]
+        unique_words  = list(document_tfidf.keys())
+
+        return unique_words
+
+    def _filter_stop_words(self, unique_words):
+        # go through each unique word and filter the stop words
+        filtered_words = [word for word in unique_words if not word in stop_words]
+        return filtered_words
+
+    def _get_mean_word_embeddings(self, text, tfidf, unique_words):
+        words_to_embeddings = {word: [] for word in unique_words}
+	    # perform word_embeddinng on the batch
+        # start = time.time()
+        embedding_dicts = compute_bert_word_embedding(self.bert_model, self.bert_tokenizer, text, unique_words, self.word_embedding_size)
+        # finish = time.time()
+        # print("Compute bert word embedding time {}".format(finish - start))
+        for i, word_embeddings_dict in enumerate(embedding_dicts):
+            tfidf_words = list(tfidf[str(i)].keys())
+            # assuming we have a list of tokens and a list of word embeddings
+            for j, token in enumerate(tfidf_words):
+                if token in word_embeddings_dict and token in words_to_embeddings:
+                    word_embedding = word_embeddings_dict[token]
+                    words_to_embeddings[token].append(word_embedding)
+        # mean each list
+        mean_embeddings = {word: None for word in unique_words}
+        for word in unique_words:
+            if word in words_to_embeddings and len(words_to_embeddings[word]) > 0:
+                embeddings = torch.stack(words_to_embeddings[word])
+                mean_word_embedding = torch.mean(embeddings, dim=0)
+                mean_embeddings[word] = mean_word_embedding
+            else:
+                mean_embeddings[word] = torch.zeros(self.word_embedding_size).to(device)
+
+        return mean_embeddings
+
+    def _make_edges(self, word_to_node_index, sentence_to_node_index,  tfidf, filtered_words):
+        # edge_attr is the tfidf features
+        edge_attr = []
+        # edge_index is of shape (2, num_edges) in COO format
+        edge_index = []
+        # Undirected edges have two edges for each direction
+        for sentence_index in tfidf.keys():
+            sentence_node_index = sentence_to_node_index[sentence_index]
+            # get sentence tfidf ict
+            tfidf_dict = tfidf[sentence_index]
+            sentence_words = tfidf_dict.keys()
+            for word in sentence_words:
+                if not word in filtered_words:
+                    continue
+                # make an edge connecting the word to the current sentence
+                word_node_index = word_to_node_index[word]
+                # make two sided edge
+                direction_one = [word_node_index, sentence_node_index]
+                direction_two = [sentence_node_index, word_node_index]
+                edge_index.append(direction_one)
+                edge_index.append(direction_two)
+                # add the corresponding attribute
+                attribute = tfidf_dict[word]
+                edge_attr.append(attribute)
+                edge_attr.append(attribute)
+        # convert to numpy
+        edge_index = torch.LongTensor(edge_index).T
+        edge_attr = torch.FloatTensor(edge_attr).unsqueeze(-1)
+        return edge_index, edge_attr
+
+    """
+        Make node attributes from word embeddings and sentence embeddings
+    """
+    def _make_nodes(self, word_embeddings, sentence_embeddings):
+        word_embedding_values = torch.stack(list(word_embeddings.values()))
+        word_embedding_values = word_embedding_values.to(device)
+        word_embedding_keys = list(word_embeddings.keys())
+        num_words = len(word_embedding_keys)
+        num_sentences = np.shape(sentence_embeddings)[0]
+        # make sure word and sentence embeddings have the same shape
+        assert np.shape(word_embedding_values)[-1] == np.shape(sentence_embeddings)[-1]
+        # sentences are first then words
+        attribute_matrix = torch.cat((sentence_embeddings, word_embedding_values), dim=0)
+        # output is of shape (num_nodes, num_node_features)
+        # make sentence to index map
+        sentence_to_node_index = {str(i): i for i in range(num_sentences)}
+        # make word to node index map
+        word_to_node_index = {word_embedding_keys[i]: i + num_sentences for i in range(num_words)}
+
+        return attribute_matrix, word_to_node_index, sentence_to_node_index
+
     def construct_graph(self, tfidf, label):
-        # sanity check
-        self._check_word_exists(tfidf, label)
         # label has the shape
         # {
         #    "name": "cluster name",
@@ -377,7 +452,7 @@ class DUCGraphConstructor(GraphConstructor):
         #       "0": {"word": tfidf}
         #   } 
         # }
-        # Get each unique word in the document
+        # Get each unique word in the document cluster
         unique_words = self._get_unique_words(tfidf)
         # Filter stop words
         filtered_unique_words = self._filter_stop_words(unique_words)
@@ -385,9 +460,11 @@ class DUCGraphConstructor(GraphConstructor):
         word_embeddings = self._get_mean_word_embeddings(label["text"], tfidf, filtered_unique_words)
         # Make a list of sentence embeddings (num_sentences, embedding_size)
         sentence_embeddings = compute_bert_sentence_embedding(label["text"], self.sentence_transformer)
+        # Make document embeddings
+        document_embeddings = self._get_document_embeddings()
         # Make node attributes
-        node_attributes, word_to_node_index, sentence_to_node_index = self._make_nodes(word_embeddings,
-                                                                                       sentence_embeddings)
+        node_attributes, word_to_node_index, sentence_to_node_index, document_to_node_index = self._make_nodes(word_embeddings,
+                                                                                                            sentence_embeddings)
         # Make edges
         edge_index, edge_attributes = self._make_edges(word_to_node_index,
                                                        sentence_to_node_index,

src/ot_coarsening/evaluation.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append(os.environ["GRAPH_SUM"])
 from src.ot_coarsening.dataset_management.graph_constructor import CNNDailyMailGraphConstructor
-from src.ot_coarsening.dataset_management.cnn_daily_mail import CNNDailyMail
+from src.ot_coarsening.dataset_management.cnndm.cnn_daily_mail import CNNDailyMail
 import collections
 import wandb
 
@@ -80,7 +80,7 @@ def get_coarse_sentences(ground_truth, coarse_indices):
     coarse_sentences = [text[sentence_index] for sentence_index in sentence_indices]
     return coarse_sentences, sentence_indices
 
-def generate_predicted_summary(model, example_graph, ground_truth, num_output_sentences):
+def generate_predicted_summary(model, example_graph, ground_truth, num_output_sentences, dense=False):
     """
         Generates a textual summmary of the example_graph using
         the given model.
@@ -95,7 +95,10 @@ def generate_predicted_summary(model, example_graph, ground_truth, num_output_se
     #example_graph.y = example_graph.y.unsqueeze(0) 
     #example_graph.mask = example_graph.mask.unsqueeze(0)
     #example_graph.adj = example_graph.adj.unsqueeze(0)
-    _, _, _, _, coarse_indices = model.forward(example_graph) 
+    if dense:
+        _, _, _, _, coarse_indices = model(example_graph, num_output_sentences=num_output_sentences)
+    else:
+        _, _, _, _, _, coarse_indices = model(example_graph, num_output_sentences = num_output_sentences)
     # should be a 1D tensor of indices
     coarse_indices = coarse_indices[0]
     predicted_summary, sentence_indices = get_coarse_sentences(ground_truth, coarse_indices)
@@ -117,7 +120,7 @@ def save_histogram(saved_sentence_inds, file_path):
     plt.show()
     plt.savefig(file_path)
             
-def perform_rouge_evaluations(model, dataset, serialize=True, log=True):
+def perform_rouge_evaluations(model, dataset, serialize=True, log=True, num_output_sentences=3, dense=False):
     """
         Perform rouge-1, rouge-2, and rouge-l evaluation given a
         trained OTCoarsening model on the given evaluation dataset.
@@ -135,7 +138,6 @@ def perform_rouge_evaluations(model, dataset, serialize=True, log=True):
     label_accuracies = []
     saved_sentence_inds = []
     ground_truth_labels = []
-    num_output_sentences = dataset.num_output_sentences
     for example_index in range(len(dataset)):
         # get the example graph
         example_graph = dataset[example_index]
@@ -149,7 +151,7 @@ def perform_rouge_evaluations(model, dataset, serialize=True, log=True):
             continue
         true_summaries.append(true_summary)
         # get summary prediction
-        predicted_summary, sentence_indices = generate_predicted_summary(model, example_graph, ground_truth, num_output_sentences)
+        predicted_summary, sentence_indices = generate_predicted_summary(model, example_graph, ground_truth, num_output_sentences, dense=False)
         saved_sentence_inds.append(np.array(sentence_indices) / len(ground_truth["text"]))
         label_accuracy = compute_label_accuracy(sentence_indices, example_label)
         label_accuracies.append(label_accuracy)

src/ot_coarsening/ot_coarsening.py
@@ -22,18 +22,18 @@ class CoarsenBlock(torch.nn.Module):
     def reset_parameters(self):
         self.gcn_att.reset_parameters()
 
-    def get_topk_range_for_sentences(self, alpha_vec, output_node_count, num_sentences):
-        assert num_sentences >= output_node_count
+    def get_topk_range_for_sentences(self, alpha_vec, num_output_sentences, num_sentences):
+        assert num_sentences >= num_output_sentences
         alpha_vec = alpha_vec.detach().cpu().numpy()
         full_argsort = np.argsort(alpha_vec)[::-1]
         topk_ind = [] 
-        num_sentences = 0
+        num_sentences_saved = 0
         for index in full_argsort:
-            if num_sentences == output_node_count:
+            if num_sentences_saved == num_output_sentences:
                 break
             is_sentence = index < num_sentences 
             if is_sentence:
-                num_sentences += 1
+                num_sentences_saved += 1
             topk_ind.append(index)
         
         temptopk = torch.Tensor(alpha_vec[topk_ind])
@@ -101,11 +101,12 @@ class CoarsenBlock(torch.nn.Module):
         return graph
     
     #@profile_every(1)    
-    def single_graph_forward(self, alpha_vec, graph, output_node_count=None, num_sentences=None):
+    def single_graph_forward(self, alpha_vec, graph, num_output_sentences=3):
         """
             Performs forward pass for a single graph
         """
         # convert the graph to dense
+        num_sentences = graph.num_sentences.item()
         dense_graph = self.convert_sparse_to_dense(graph)
         adj = dense_graph.adj.squeeze().float()
         x = dense_graph.x
@@ -115,15 +116,9 @@ class CoarsenBlock(torch.nn.Module):
         norm_adj = self.normalize_batch_adj(adj.unsqueeze(0)).squeeze()
         # get topk
         cut_value = 0
-        if num_sentences is None:
-            # This is in the train phase
-            num_coarse_nodes = int(num_nodes * self.assign_ratio) + 1
-            temptopk, topk_ind = alpha_vec.topk(num_coarse_nodes, dim=-1)
-            cut_value = temptopk[-1]
-        else:
-            # This is in the evaluation phase
-            temptopk, topk_ind = self.get_topk_range_for_sentences(alpha_vec, output_node_count, num_sentences)
-            cut_value = temptopk[-1]
+        # This is in the evaluation phase
+        temptopk, topk_ind = self.get_topk_range_for_sentences(alpha_vec, num_output_sentences, num_sentences)
+        cut_value = temptopk[-1]
         # calculate S
         cut_alpha_vec = torch.relu(alpha_vec + 0.0000001 - cut_value)
         repeated_cut_alpha_vec = cut_alpha_vec.repeat(cut_alpha_vec.shape[0], 1)
@@ -141,7 +136,7 @@ class CoarsenBlock(torch.nn.Module):
 
         return sparse_graph, S, topk_ind
 
-    def forward(self, data, num_sentences=None, output_node_counts=None):
+    def forward(self, data, num_output_sentences=3):
         # compute attention scores
         alpha_vec = self.calculate_attention(data)
         # convert batch to list
@@ -153,20 +148,13 @@ class CoarsenBlock(torch.nn.Module):
         Ss = []
         for j in range(batch_size):
             # prepare data for individual forward pass
-            if output_node_counts is None:
-                output_node_count = None
-                current_num_sentences = None
-            else:
-                output_node_count = output_node_counts[j].int().item()
-                current_num_sentences = num_sentences[j]
             current_graph = graph_list[j]
             # prepare alpha vec 
             current_alpha_vec = alpha_vec[j]
             # run forward pass for this single graph
             sparse_graph, S, topk_ind = self.single_graph_forward(current_alpha_vec,
                                                                   current_graph, 
-                                                                  output_node_count=output_node_count, 
-                                                                  num_sentences=current_num_sentences)
+                                                                  num_output_sentences=num_output_sentences)
             # add to lists
             Ss.append(S)
             sparse_graphs.append(sparse_graph)
@@ -193,7 +181,8 @@ class Coarsening(torch.nn.Module):
         self.embed_block1.reset_parameters()
         self.coarse_block1.reset_parameters()
 
-    def compute_loss(self, data, coarse_data, epsilon, opt_epochs, p, num_sentences):
+    def compute_loss(self, data, coarse_data, epsilon, opt_epochs, p):
+        num_sentences = data.num_sentences
         opt_loss = 0.0
         data_list = data.to_data_list()
         coarse_list = coarse_data.to_data_list()
@@ -220,7 +209,7 @@ class Coarsening(torch.nn.Module):
         return new_batch
 
     #@profile_every(1)
-    def forward(self, data, p=2, output_node_counts=None, num_sentences=None):
+    def forward(self, data, p=2, num_output_sentences=3):
         data_copy = self.batch_copy(data)
         x, edge_index, edge_attr = data.x, data.edge_index, data.edge_attr
         batch_num_nodes = x.shape[0]
@@ -238,8 +227,7 @@ class Coarsening(torch.nn.Module):
         data.x = x1
         # convert the data to dense for this phase
         coarse_batch, S, batch_topk_ind = self.coarse_block1(data,
-                                                             num_sentences=num_sentences,
-                                                             output_node_counts=output_node_counts)
+                                                             num_output_sentences=num_output_sentences)
 
         coarse_x = coarse_batch.x
         xs = [coarse_x.mean(dim=1)]
@@ -253,7 +241,7 @@ class Coarsening(torch.nn.Module):
             x2 = torch.relu(uncompressed_embeddings)
         coarse_batch.x = x2
         xs.append(x2.mean(dim=1))
-        opt_loss = self.compute_loss(data_copy, coarse_batch, self.epsilon, self.opt_epochs, p, num_sentences)
+        opt_loss = self.compute_loss(data_copy, coarse_batch, self.epsilon, self.opt_epochs, p)
 
         return xs, edge_index, edge_attr, S, opt_loss, batch_topk_ind
 